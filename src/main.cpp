655524ebd7abb4b7c30f8f376dfcfa9752567101
Ollie Ford <me@ojford.com>
2014-11-27 16:36:25 +0000

    project init

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LRU-CacheSim
+//
+//  Created by Ollie Ford on 25/11/2014.
+//  Copyright (c) 2014 OJFord. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
