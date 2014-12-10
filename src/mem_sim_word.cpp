6dc590611eefb206c338f1e5a90a42e83b0dadc4
Ollie Ford <me@ojford.com>
2014-12-10 00:21:16 +0000

    improved interface

--- src/mem_sim_word.cpp
+++ src/mem_sim_word.cpp
@@ -9,30 +9,30 @@
 #include "mem_sim_word.h"
 #include "mem_sim_exceptions.h"
 
-Word::Word(unsigned len, bool zero): bytes(len, 0){
+Word::Word(unsigned len, bool zero): fvec<uint8_t>(len, 0){
 	if(zero)
-		for(unsigned i=0; i<bytes.size(); ++i)
-			bytes.at(i) = 0;
+		for(unsigned i=0; i<fvec<uint8_t>::size(); ++i)
+			fvec<uint8_t>::at(i) = 0;
 }
-Word::Word(unsigned len, uint8_t* ibuf): bytes(len, 0){
-	for(unsigned i=0; i<bytes.size(); ++i)
-		bytes.at(i) = ibuf[i];
+Word::Word(unsigned len, uint8_t* ibuf): fvec<uint8_t>(len, 0){
+	for(unsigned i=0; i<fvec<uint8_t>::size(); ++i)
+		fvec<uint8_t>::at(i) = ibuf[i];
 }
 
 Word::~Word(void){
 }
 
 fvec<uint8_t> Word::get(void) const{
-	return bytes;
+	return (fvec<uint8_t>)*this;
 }
 
 void Word::set(fvec<uint8_t> idata){
-	if( idata.size() != bytes.size() )
+	if( idata.size() != fvec<uint8_t>::size() )
 		throw SizeMismatchException;
 	
-	bytes = idata;
+	fvec<uint8_t>::operator=(idata);
 }
 
 void Word::set(Word copy){
-	bytes = copy.bytes;
+	fvec<uint8_t>::operator=(copy);
 }
